2ceed7d5645d792c7b28725f339acb99c3c31e85
Prince_codes <princeotsetswe79@gmail.com>
2026-04-01 13:33:00 +0200

    Create rock, paper, scissors game.py

---
+++ rock, paper, scissors game.py
@@ -0,0 +1,30 @@
+import random
+choices = ['rock', 'paper', 'scissors']
+computer_choice = random.choice(choices)
+name = input('''Hi there, Input Player name: 
+''')
+user_choice=''
+attempts=0
+
+while user_choice !=computer_choice:
+    user_choice= input(f'Rock, Paper, or Scissors? {name} ')
+    attempts +=1
+    if user_choice.lower == computer_choice:
+        print(f'Its a tie! try again {name}')
+    elif user_choice == 'rock' and computer_choice== 'paper':
+         print(f'You lose! try again {name}')
+    elif user_choice == 'rock' and computer_choice== 'scissors':
+         print(f'''You win! {name}...Number of attempts: {attempts}''')
+         break
+    elif user_choice == 'paper' and computer_choice== 'rock':
+         print(f'''You win! {name}...Number of attempts: {attempts} ''')
+         break
+    elif user_choice == 'paper' and computer_choice== 'scissors':
+         print(f'You lose! try again {name}')
+    elif user_choice == 'scissors' and computer_choice== 'paper':
+         print(f'''You win! {name}..Number of attempts: {attempts}''')
+         break
+    elif user_choice == 'scissors' and computer_choice== 'rock':
+         print(f'You lose! try again {name}')
+    else:
+        print(f"Sorry!! but i don't understand your choice {name}")
