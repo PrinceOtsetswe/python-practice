e75166f877a02aee555e05c713b8418a9583fbcc
Prince_codes <princeotsetswe79@gmail.com>
2026-05-05 22:38:18 +0200

    Update rock, paper, scissors game.py

--- rock, paper, scissors game.py
+++ rock, paper, scissors game.py
@@ -9,7 +9,7 @@ attempts=0
 while user_choice !=computer_choice:
     user_choice= input(f'Rock, Paper, or Scissors? {name} ')
     attempts +=1
-    if user_choice.lower == computer_choice:
+    if user_choice.lower() == computer_choice:
         print(f'Its a tie! try again {name}')
     elif user_choice == 'rock' and computer_choice== 'paper':
          print(f'You lose! try again {name}')
